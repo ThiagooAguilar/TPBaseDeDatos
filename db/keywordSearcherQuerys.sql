c3d74b64bbb6b89bef7bf63f99873610b19159b3
LucioZampella <lzluciozampella@gmail.com>
2025-10-28 11:10:13 -0300

    Le saque el id del select porque no interesa que la pagina lo muestre despues

--- db/keywordSearcherQuerys.sql
+++ db/keywordSearcherQuerys.sql
@@ -5,18 +5,18 @@
 
 --TABLA QUE BUSCA LAS PELICULAS ASOCIADAS A ESA KEYWORD
 
-WITH keyword_in_movie_table AS (SELECT m.*, k.keyword_id, k.keyword_name
+WITH keyword_in_movie_table AS (SELECT m.*, k.keyword_name
 FROM movie m
          LEFT JOIN movie_keywords mk ON m.movie_id = mk.movie_id
          LEFT JOIN keyword k ON mk.keyword_id = k.keyword_id)
 
 SELECT *
-FROM keyword_in_movie_table
+FROM keyword_in_movie_table;
 --WHERE keyword_name ILIKE $1; LO COMENTO PARA QUE NO TIRE ERROR AL PUSHEAR
 
 --EJEMPLO: LAS PELICULAS QUE TIENE LA KEYWORD italy
 
-WITH keyword_in_movie_table AS (SELECT m.*, k.keyword_id, k.keyword_name
+WITH keyword_in_movie_table AS (SELECT m.*, k.keyword_name
                                 FROM movie m
                                          LEFT JOIN movie_keywords mk ON m.movie_id = mk.movie_id
                                          LEFT JOIN keyword k ON mk.keyword_id = k.keyword_id)
